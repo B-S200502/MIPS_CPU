abb901b4c9ee96f0b7ad2979596cc3973f01faa5
Basem Sousou <123136820+B-S200502@users.noreply.github.com>
2025-11-17 12:53:08 -0500

    Refactor D_cache entity and architecture

--- lab4_basem/D_cache.vhd
+++ lab4_basem/D_cache.vhd
@@ -1,43 +1,35 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
-use IEEE.std_logic_unsigned.all;
+use IEEE.std_logic_1164.all;
+use IEEE.NUMERIC_STD.all;
 
 entity d_cache is
-	port( 
-		din : in std_logic_vector(31 downto 0);
-		reset : in std_logic;
-		clk : in std_logic;
-		write : in std_logic;
-		address : in std_logic_vector(4 downto 0);
-		d_out : out std_logic_vector(31 downto 0));
-end d_cache ;
+	port(	d_in      : in std_logic_vector(31 downto 0);
+		data_write: in std_logic;
+		clk       : in std_logic;
+		reset     : in std_logic;
+		destination_reg  : in std_logic_vector(4 downto 0);
+		d_out     : out std_logic_vector(31 downto 0));
+end d_cache;
 
-architecture arch of d_cache is
+architecture d_cache of d_cache is
 
-    type t_reg_array is array (0 to 31) of std_logic_vector(31 downto 0);
-    signal reg_array  : t_reg_array;
+type memory_location is array(0 to 31) of std_logic_vector(31 downto 0);
+signal cache_memory : memory_location;
 
 begin
+	d_out <= cache_memory (TO_INTEGER(unsigned(destination_reg)));
 
-    writing : process( clk,reset )
-    begin
-        if (reset = '1') then
-            for i in 0 to 31 loop
-                reg_array(i) <= "00000000000000000000000000000000";
-            end loop ;
-        
-        elsif (clk'event and clk='1') then
-            if (write='1') then -- only write under this condition
-                reg_array(conv_integer(address)) <= din;
-            end if;
-
-        end if;
-    end process; 
-
-
-    reading : process( address,reg_array )
-    begin
-    	d_out <= reg_array(conv_integer(address));
-    end process;
-
-end arch ;
+process(d_in, reset, clk, data_write, destination_reg)
+begin
+	if(reset ='1') then
+		for i in cache_memory'range loop
+			cache_memory(i) <= (others => '0');
+		end loop;
+	elsif (rising_edge(clk)) then
+		if(data_write = '1') then
+			cache_memory(TO_INTEGER(unsigned(destination_reg))) <= d_in;
+		end if;
+	end if;
+end process;
+end d_cache;
